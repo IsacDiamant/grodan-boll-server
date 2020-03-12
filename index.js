8b0a2272af5432c66f10533da229ab4052e631b8
Isac Diamant <isac.c.diamant@gmail.com>
2020-03-12 11:24:40 +0100

    uploading the server to github

---
+++ index.js
@@ -0,0 +1,11 @@
+var io = require('socket.io')(process.env.PORT || 52300);
+
+console.log('Server has started');
+
+io.on('connection', function(socket) {
+    console.log('Connection made!');
+
+    socket.on('disconnect', function(){
+        console.log('A player has disconnected');
+    });
+});
